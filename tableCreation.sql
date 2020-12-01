5594fe814872a2cb8dc5ab1084c3a6b0a5944ddb
Delacourt <remi.delacourt@gmail.com>
2020-12-01 09:56:40 +0100

    created first version of tables

---
+++ tableCreation.sql
@@ -0,0 +1,70 @@
+create table program(
+    id serial primary key, 
+    name varchar(10) not null,
+    year smallint not null
+);
+
+create table categories(
+    id serial primary key,
+    description varchar not null,
+);
+
+create table internship(
+    id serial primary key,
+    title varchar not null,
+    creation_date date not null,
+    content text not null,
+    supervisor_id int, foreign key (supervisor_id) references person(id),
+    scientific_validated boolean not null,
+    administr_validated boolean not null,
+    is_taken boolean not null,
+    program_id int, foreign key (program_id) references program(id)
+);
+
+create table internship_category(
+    id serial primary key,
+    internship_id int, foreign key (internship_id) references internship(id),
+    category_id int, foreign key (category_id) references categories(id),
+);
+
+create table person(
+    id serial primary key,
+    name varchar(128) not null,
+    email varchar(128) not null,
+    creation_date date not null,
+    valid boolean not null,
+    password text not null, check length(password) >= 8
+);
+
+create table role_type(
+    id serial primary key,
+    role varchar(32)
+);
+
+create table person_roles(
+    id serial primary key,
+    role_id int, foreign key (role_id) references role_type(id),
+    person_id int, foreign key (person) references person(id)
+);
+
+create table error(
+    id serial primary key,
+    date date not null,
+    time timestamp not null,
+    method_raised varchar(128) not null,
+    description text,
+    person_id int, foreign key (person_id) references person(id)
+);
+
+create table person_internship(
+    id serial primary key,
+    internship_id int, foreign key (internship_id) references internship(id),
+    person_id int unique, foreign key (person) references person(id)
+);
+
+create table person_program(
+    id serial primary key,
+    program_id int, foreign key (program_id) references program(id),
+    person_id int, foreign key (person) references person(id)
+);
+
