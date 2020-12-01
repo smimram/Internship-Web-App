3fc28348f3fe809271884a2199c51aa350b622df
Delacourt <remi.delacourt@gmail.com>
2020-12-01 10:29:14 +0100

    corrected table creations & inserted Ioana as Admin

--- tableCreation.sql
+++ tableCreation.sql
@@ -8,7 +8,16 @@ create table program(
 
 create table categories(
     id serial primary key,
-    description varchar not null,
+    description varchar not null
+);
+
+create table person(
+    id serial primary key,
+    name varchar(128) not null,
+    email varchar(128) not null,
+    creation_date date not null,
+    valid boolean not null,
+    password text not null check (length(password) >= 8)
 );
 
 create table internship(
@@ -26,16 +35,7 @@ create table internship(
 create table internship_category(
     id serial primary key,
     internship_id int, foreign key (internship_id) references internship(id),
-    category_id int, foreign key (category_id) references categories(id),
-);
-
-create table person(
-    id serial primary key,
-    name varchar(128) not null,
-    email varchar(128) not null,
-    creation_date date not null,
-    valid boolean not null,
-    password text not null, check length(password) >= 8
+    category_id int, foreign key (category_id) references categories(id)
 );
 
 create table role_type(
@@ -46,7 +46,7 @@ create table role_type(
 create table person_roles(
     id serial primary key,
     role_id int, foreign key (role_id) references role_type(id),
-    person_id int, foreign key (person) references person(id)
+    person_id int, foreign key (person_id) references person(id)
 );
 
 create table error(
@@ -61,13 +61,13 @@ create table error(
 create table person_internship(
     id serial primary key,
     internship_id int, foreign key (internship_id) references internship(id),
-    person_id int unique, foreign key (person) references person(id)
+    person_id int unique, foreign key (person_id) references person(id)
 );
 
 create table person_program(
     id serial primary key,
     program_id int, foreign key (program_id) references program(id),
-    person_id int, foreign key (person) references person(id)
+    person_id int, foreign key (person_id) references person(id)
 );
 
 
@@ -81,4 +81,4 @@ insert into role_type(role) values ('Admin');
 
 insert into person(name, email, creation_date, valid, password) values ('Manolescu, Ioana', 'ioana.manolescu@inria.fr', current_date, 'true', 'iloveDBMS');
 
---insert into person_roles(role_id, person_id) values ();
+insert into person_roles(role_id, person_id) values (5, 1);
