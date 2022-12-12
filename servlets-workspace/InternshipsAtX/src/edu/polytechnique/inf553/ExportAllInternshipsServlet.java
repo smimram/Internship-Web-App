57d1dd64df73d2fe3c7964693aaa1650e37786aa
Nelly Barret <nelly.barret@inria.fr>
2022-12-12 15:14:48 +0100

    replace COPY query by JDBC copy manager

--- servlets-workspace/InternshipsAtX/src/edu/polytechnique/inf553/ExportAllInternshipsServlet.java
+++ servlets-workspace/InternshipsAtX/src/edu/polytechnique/inf553/ExportAllInternshipsServlet.java
@@ -4,18 +4,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.io.*;
+import java.sql.*;
 import java.util.Base64;
 import java.nio.file.Paths;
 import java.io.File;
 import java.util.Arrays;
 import java.io.FileInputStream;
+import org.postgresql.copy.CopyManager;
+import org.postgresql.core.BaseConnection;
 
 public class ExportAllInternshipsServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -32,23 +29,26 @@ public class ExportAllInternshipsServlet extends HttpServlet {
                 response.sendError(HttpServletResponse.SC_FORBIDDEN);
             }
 
-            // CREATE TABLE exports (id SERIAL, date DATE, file BYTEA);
             String filePath = Paths.get("/tmp", "exportsInternshipsData", "exportInternshipData.csv").toString();
             File f = new File(filePath);
             f.getParentFile().mkdirs();
-            String query = "\\copy (" +
-                    "  SELECT i.id AS internship_id, i.title AS internship_title, i.institution AS intersnhip_instituion, i.creation_date AS internship_creation_date, i.scientific_validated AS scientific_validation, i.administr_validated AS administrative_validation, p.name AS supervisor_name, p.email AS supervisor_email, CONCAT(pr.name, ' - ', pr.year) AS program, i.confidential_internship, p2.name AS student_name, p2.email AS student_email " +
-                    "  FROM internship i " +
-                    "  LEFT JOIN person p ON i.supervisor_id = p.id " +
-                    "  LEFT JOIN program pr ON i.program_id = pr.id " +
-                    "  LEFT JOIN person_internship pi ON pi.internship_id = i.id " +
-                    "  LEFT JOIN person p2 ON pi.person_id = p2.id" +
-                    ") " +
-                    "TO '" + filePath + "' " +
-                    "DELIMITER ',' CSV HEADER;";
-            try (PreparedStatement ps = con.prepareStatement(query)) {
-                ps.execute();
 
+                // write the csv under /tmp
+                CopyManager copyManager = new CopyManager((BaseConnection) con); // temporary copy manager
+                String query = "SELECT i.id AS internshipId, i.title AS internshipTitle, i.institution AS intersnhipInstitution, i.creation_date AS internshipCreationDate, i.scientific_validated AS scientificValidation, i.administr_validated AS administrativeValidation, p.name AS supervisorName, p.email AS supervisorEmail, CONCAT(pr.name, pr.year) AS program, i.confidential_internship, p2.name AS studentName, p2.email AS studentEmail FROM internship i LEFT JOIN person p ON i.supervisor_id = p.id LEFT JOIN program pr ON i.program_id = pr.id LEFT JOIN person_internship pi ON pi.internship_id = i.id LEFT JOIN person p2 ON pi.person_id = p2.id";
+                copyManager.copyOut("COPY ( " + query + " ) TO STDOUT WITH CSV HEADER DELIMITER ';'", new FileOutputStream(f));
+
+//                String query = "'\\copy ( SELECT i.id AS internshipId, i.title AS internshipTitle, i.institution AS intersnhipInstituion, i.creation_date AS internshipCreationDate, i.scientific_validated AS scientificValidation, i.administr_validated AS administrativeValidation, p.name AS supervisorName, p.email AS supervisorEmail, CONCAT(pr.name, pr.year) AS program, i.confidential_internship, p2.name AS studentName, p2.email AS studentEmail FROM internship i LEFT JOIN person p ON i.supervisor_id = p.id LEFT JOIN program pr ON i.program_id = pr.id LEFT JOIN person_internship pi ON pi.internship_id = i.id LEFT JOIN person p2 ON pi.person_id = p2.id) TO /tmp/exports/export.csv'";
+//                System.out.println(query);
+//                ProcessBuilder builderLoad = new ProcessBuilder("psql", "-d", "internship_webapp", "-c", query);
+//                System.out.println(builderLoad.command());
+//                Process processLoad = builderLoad.inheritIO().start(); // builderLoad.start()
+//                processLoad.waitFor();
+//                if (processLoad.exitValue() != 0) {
+//                    throw new IllegalStateException("Something went wrong while exporting the CSV file.");
+//                }
+
+                // write the file as bytes (to be sent to the JSP)
                 FileInputStream fl = new FileInputStream(f);
                 byte[] contentBytes = new byte[(int)f.length()];
                 fl.read(contentBytes);
@@ -56,7 +56,6 @@ public class ExportAllInternshipsServlet extends HttpServlet {
                 String encodedContent = Base64.getEncoder().encodeToString(contentBytes);
                 request.setAttribute("encodedContent", encodedContent);
                 returnFileName = "download_complete_export.jsp";
-            }
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
